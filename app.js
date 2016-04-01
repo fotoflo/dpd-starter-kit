c2c4993f4fe5562482a7622bddaea8c9e5f46f50
Alex Miller <fotoflo@gmail.com>
2016-03-31 18:35:26 -0700

    inital commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var _ = require('lodash');
+var getenv = require('getenv');
+
+config = {
+    'dpdServerRoot' :    getenv('R4_DPD_SERVER_ROOT'),
+    'dpd_port'    :   getenv.int('R4_DPD_PORT'),
+    'dpd_env'   :   getenv('R4_DPD_ENV')
+  };
+
+  init(config);
+
+lodash = require('lodash');
+moment = require('moment');
+request = require('request-promise');
+
+function init(config){
+
+  console.log("Welcome to dpd-texturner!  Config loaded!");
+
+  _.forEach(config, function(n, key){
+    console.log(key, " : ",  n);
+  });
+
+  var deployd = require('deployd');
+  var deploydOptions = {
+    port: config.dpd_port,
+    env: config.dpd_env
+  };
+  var dpd = deployd(deploydOptions);
+  dpd.listen();
+
+  dpd.on('listening', function() {
+    console.log("Deployd is listening on port ", deploydOptions.port);
+  });
+
+  dpd.on('error', function(err) {
+    console.error(err);
+    process.nextTick(function() { // Give the server a chance to return an error
+      process.exit();
+    });
+  });
+
+  console.log("");
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# r4 node project
+
+## install dependancies
+npm install
+
+## install env
+copy env/dev and run it in the console
+
+## start app
+node app.js
+
+
